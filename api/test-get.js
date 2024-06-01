9c64fcc3a3355619fb97e2ed14bbf7ed54d54fb7
DmitryFlow <dimos548rus@gmail.com>
2024-06-01 13:41:24 +0200

    Ver9

--- api/test-get.js
+++ api/test-get.js
@@ -9,7 +9,7 @@ export default function handler(req, res) {
 	}
 
 	if (req.method === 'GET') {
-		res.status(200).json({ message: 'GET request successful!' });
+		res.status(200).json({ message: 'GET successful!' });
 	} else {
 		res.setHeader('Allow', ['GET']);
 		res.status(405).end(`Method ${req.method} Not Allowed`);
