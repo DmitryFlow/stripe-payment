61770b773bdc9f6d592ea5d528f64961aff05b60
DmitryFlow <dimos548rus@gmail.com>
2024-06-02 13:48:39 +0200

    Ver19

---
+++ api/express-server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import request from 'request';
+
+const app = express();
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', 'https://allurepremiumservice.com');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
+app.get('/api/test-get', (req, res) => {
+  res.json({ message: 'GET request successful!' });
+});
+
+app.get('/jokes/random', (req, res) => {
+  request(
+    { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+export default app;
--- api/test-get.js
+++ api/test-get.js
@@ -1,14 +1,9 @@
 export default function handler(req, res) {
-	//res.setHeader('Access-Control-Allow-Origin', 'https://allurepremiumservice.com');
-	//res.setHeader('Access-Control-Allow-Origin', '*');
-	//res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-	//res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
 	if (req.method === 'OPTIONS') {
 		res.status(200).end();
 		return;
 	}
-
+	
 	if (req.method === 'GET') {
 		res.status(200).json({ message: 'GET successful!' });
 	} else {
@@ -16,4 +11,8 @@ export default function handler(req, res) {
 		res.status(405).end(`Method ${req.method} Not Allowed`);
 	}
 }
-  
+
+//res.setHeader('Access-Control-Allow-Origin', 'https://allurepremiumservice.com');
+//res.setHeader('Access-Control-Allow-Origin', '*');
+//res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+//res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
